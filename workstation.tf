a6b34f18844ac7f0144e728c95c3fb770c5c37ce
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-02-23 18:34:11 +0530

    eks

---
+++ workstation.tf
@@ -0,0 +1,56 @@
+module "workstation" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "workstation-eksctl"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_eksctl.id]
+  subnet_id              = "subnet-0482ced596e07afe1"
+  user_data = file("workstation.sh")
+  tags = {
+      Name = "workstation"
+    }
+}
+
+resource "aws_security_group" "allow_eksctl" {
+  name        = "eksctl"
+  description = "Created for eksctl"
+
+  tags = {
+    Name = "allow_tls"
+  }
+ 
+  ingress {
+    description = "All ports"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "centos8"{
+    owners = ["973714476881"]
+    most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
